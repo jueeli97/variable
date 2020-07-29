3b2eeac32066e30185cec176da34b999c55a525c
unknown <jueeli97@gmail.com>
2020-07-29 23:48:30 +0530

    variable
    Signed-off-by: unknown <jueeli97@gmail.com>

---
+++ index.js
@@ -0,0 +1,42 @@
+// variable
+
+// let keyword
+let intValue = 10;
+console.log(intValue);
+
+// Exception case
+//console.log(intValue1);   Cannot access 'intValue1' before initialization
+ //let intValue1= 20;
+
+
+
+
+//var keyword  
+ var intValue2 = 25;
+ console.log(intValue2);
+
+//var intValue = 20;
+//console.log(intValue); // Identifier 'intValue' has already been declared
+
+ intValue2 = 30; // once initialized var, it has scope throughout file.
+ console.log(intValue2);
+
+// exception case
+ console.log(intValue3); // in output we get undefined
+ var intValue3 =40;
+  
+
+// const keyword
+// one time declaration. It's value remains same throughout program.
+ const pi = 3.14;
+ console.log(pi);
+
+//exception case
+// pi = 4.5;
+// console.log(pi); // Assignment to constant variable.
+
+
+//console.log(int);  //Cannot access 'int' before initialization
+//const int = 3;
+ 
+
